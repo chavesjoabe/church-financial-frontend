fb299388b20f38897d4bb516ec1e590958190d28
JoabeChaves <chavesjoabe1@gmail.com>
2025-03-31 17:45:17 -0300

    feat: add onclick behavior to home page cards

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -13,14 +13,21 @@ import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import MoneyOffIcon from '@mui/icons-material/MoneyOff';
 import PersonIcon from '@mui/icons-material/Person';
 import SummarizeIcon from '@mui/icons-material/Summarize';
+import { UserRoles } from "../../constants/user.roles";
 
 interface GridItemProps {
   icon: SvgIconComponent;
   title: string;
   subtitle: string;
+  onClick: () => void;
 }
 
-const GridItem: React.FC<GridItemProps> = ({ icon: Icon, title, subtitle }) => {
+type Props = {
+  handleChange: (event: React.SyntheticEvent, value: number) => void;
+  userRole: string;
+}
+
+const GridItem: React.FC<GridItemProps> = ({ icon: Icon, title, subtitle, onClick }) => {
   return (
     <Paper
       sx={{
@@ -32,7 +39,9 @@ const GridItem: React.FC<GridItemProps> = ({ icon: Icon, title, subtitle }) => {
         '&:hover': {
           transform: 'scale(1.05)', // Scale slightly when hovered
         }
-      }}>
+      }}
+      onClick={onClick}
+    >
       {Icon && <Icon fontSize='large' color='primary' />}
       <Typography fontWeight='bold' variant="button" marginTop='10px'>
         {title}
@@ -44,7 +53,7 @@ const GridItem: React.FC<GridItemProps> = ({ icon: Icon, title, subtitle }) => {
   )
 }
 
-export const HomeCard: React.FC = () => {
+export const HomeCard: React.FC<Props> = ({ handleChange, userRole }) => {
   return (
     <Box
       width='100%'
@@ -69,6 +78,7 @@ export const HomeCard: React.FC = () => {
                 icon={AttachMoneyIcon}
                 title='Lançamento de Entradas'
                 subtitle='Para lançar uma nova entrada clique na aba ENTRADA DE VALORES e preencha os campos do formulário'
+                onClick={() => handleChange(null as unknown as React.SyntheticEvent, 1)}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -77,24 +87,37 @@ export const HomeCard: React.FC = () => {
                 icon={MoneyOffIcon}
                 title='Lançamento de saídas'
 
-                subtitle='Para lançar uma nova entrada clique na aba SAÍDA DE VALORES e preencha os campos do formulário' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <
-                GridItem
-                icon={PersonIcon}
-                title='Criação de Usuários'
-                subtitle='É possível criar novos usuários para compor a equipe de tesouraria'
+                subtitle='Para lançar uma nova entrada clique na aba SAÍDA DE VALORES e preencha os campos do formulário'
+                onClick={() => handleChange(null as unknown as React.SyntheticEvent, 2)}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <
-                GridItem
-                icon={SummarizeIcon}
-                title='Extração de relatórios'
+            {
+              userRole === UserRoles.ADMIN.toString() && (
+                <Grid item xs={12} sm={6}>
+                  <
+                    GridItem
+                    icon={PersonIcon}
+                    title='Criação de Usuários'
+                    subtitle='É possível criar novos usuários para compor a equipe de tesouraria'
+                    onClick={() => handleChange(null as unknown as React.SyntheticEvent, 5)}
+                  />
+                </Grid>
+              )
+            }
+            {
+              userRole === UserRoles.ADMIN.toString() && (
+                <Grid item xs={12} sm={6}>
+                  <
+                    GridItem
+                    icon={SummarizeIcon}
+                    title='Extração de relatórios'
 
-                subtitle='É possível extrair relatórios de acordo com o período solicitado' />
-            </Grid>
+                    subtitle='É possível extrair relatórios de acordo com o período solicitado'
+                    onClick={() => handleChange(null as unknown as React.SyntheticEvent, 4)}
+                  />
+                </Grid>
+              )
+            }
           </Grid>
         </CardContent>
       </Card>
